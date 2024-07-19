303e877d10429222ad4e673e3f3bbe562d75dda9
Kondratev AS <KondratevAS@pg-spb.ru>
2024-07-19 12:35:23 +0300

    протестил на винде - работает!

---
+++ with_c_module.cmd
@@ -0,0 +1,7 @@
+
+@echo off
+rem This script was created by Nuitka to execute 'with_c_module.exe' with Python DLL being found.
+set PATH=C:\Users\KONDRA~1\AppData\Local\Programs\Python\PYTHON~2;%PATH%
+set PYTHONHOME=C:\Users\KondratevAS\AppData\Local\Programs\Python\Python312
+set NUITKA_PYTHONPATH=C:\Users\KondratevAS\pyCharm_work\NuitkaStart;C:\Users\KondratevAS\AppData\Local\Programs\Python\Python312\DLLs;C:\Users\KondratevAS\AppData\Local\Programs\Python\Python312\Lib;C:\Users\KondratevAS\AppData\Local\Programs\Python\Python312;C:\Users\KondratevAS\pyCharm_work\NuitkaStart\venv;C:\Users\KondratevAS\pyCharm_work\NuitkaStart\venv\Lib\site-packages;C:\Users\KondratevAS\pyCharm_work\NuitkaStart\venv\Lib\site-packages\setuptools\_vendor
+"%~dp0with_c_module.exe" %*
--- with_c_module.py
+++ with_c_module.py
@@ -9,3 +9,6 @@ spec = importlib.util.spec_from_file_location(module_name, file_path)
 module = importlib.util.module_from_spec(spec)
 sys.modules[module_name] = module
 spec.loader.exec_module(module)
+
+print(dir(module))
+module.main()
